bc74aa88b01edf8582525e65656f520de8accf03
Kllanos2003 <kllanos@unab.edu.co>
2023-11-12 22:32:21 -0500

    Puntaje primera parte

--- Assets/scrips/colaider.cs
+++ Assets/scrips/colaider.cs
@@ -1,17 +1,106 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DestroyOnCollision : MonoBehaviour
+public class ControladorCollider : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        // Verifica si el objeto con el que colisionó tiene un componente de renderer
-        // Puedes ajustar esto según los componentes que desees verificar
-        Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+    public KeyCode teclaActiva = KeyCode.Space;
+    private Collider miCollider;
+    private bool estaActivo = false;
+    public static int puntosTotales = 0;
+    public Text textoPuntaje;
 
-        if (renderer != null)
+    void Start()
+    {
+        miCollider = GetComponent<Collider>();
+        miCollider.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(teclaActiva) && !estaActivo)
         {
-            // Destruye el objeto con el que colisionó
-            Destroy(collision.gameObject);
+            ActivarCollider();
+            Invoke("DesactivarCollider", 0.2f);
+            Invoke("VerificarColision", 0.5f);
+        }
+
+        ActualizarTextoPuntaje();
+    }
+
+    void ActivarCollider()
+    {
+        if (!estaActivo)
+        {
+            miCollider.enabled = true;
+            estaActivo = true;
+        }
+    }
+
+    void DesactivarCollider()
+    {
+        miCollider.enabled = false;
+        estaActivo = false;
+    }
+
+    void VerificarColision()
+    {
+        // Verifica si no hubo colisiones después de 0.5 segundos y resta un punto si es el caso
+        if (!ColisionDetectada())
+        {
+            // No ha habido colisión, resta un punto
+            RestarPunto();
+        }
+    }
+
+    bool ColisionDetectada()
+    {
+        // Verifica si hay algún collider en la posición actual del objeto
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.1f);
+        
+        // Excluye el propio collider del objeto actual
+        foreach (Collider col in colliders)
+        {
+            if (col != miCollider)
+            {
+                return true; // Hay una colisión
+            }
+        }
+
+        return false; // No hay colisiones
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collider ha entrado en contacto con: " + collision.gameObject.name);
+
+        // Asegúrate de que la colisión no se cuente más de una vez
+        if (estaActivo) 
+        {
+            // Aumenta la puntuación compartida cada vez que hay una colisión
+            SumarPunto();
+        }
+    }
+
+    void SumarPunto()
+    {
+        puntosTotales++;
+        Debug.Log("Punto ganado. Puntos totales: " + puntosTotales);
+        ActualizarTextoPuntaje();
+    }
+
+    void RestarPunto()
+    {
+        puntosTotales--;
+        Debug.Log("No hubo colisión después de 0.5 segundos. Punto restado. Puntos totales: " + puntosTotales);
+        ActualizarTextoPuntaje();
+    }
+
+    void ActualizarTextoPuntaje()
+    {
+        if (textoPuntaje != null)
+        {
+            textoPuntaje.text = "Puntuación: " + puntosTotales;
         }
     }
 }
+
--- Assets/scrips/destruir.cs
+++ Assets/scrips/destruir.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Destruir : MonoBehaviour
 {
-    public float posicionDestruccionY; // La posición Y en la que se destruirá la bola
+   
 
     // Este método se llama cuando este objeto colisiona con otro objeto
     private void OnCollisionEnter(Collision collision)
@@ -10,12 +10,8 @@ public class Destruir : MonoBehaviour
         // Verifica si la colisión ocurrió con algún objeto
         if (collision.gameObject != null)
         {
-            // Comprueba si la posición Y de la bola es menor o igual a la posición de destrucción
-            if (transform.position.y <= posicionDestruccionY)
-            {
-                // Destruye el objeto que colisionó
                 Destroy(gameObject);
-            }
+
         }
     }
 }
